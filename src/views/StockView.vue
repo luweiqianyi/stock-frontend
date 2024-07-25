cd6a4656ed0a890b679df99255419964f846db22
luweiqianyi <runningriven@163.com>
2024-07-25 10:46:35 +0800

    增加一个股票计算页

---
+++ src/views/StockView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="stock">
+        <h1>This is an stock page</h1>
+    </div>
+</template>
+  
+<style>
+@media (min-width: 1024px) {
+    .stock {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
